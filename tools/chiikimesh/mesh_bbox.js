e1bebc17bc50b0fa617ad4a121a77df8b4e1838d
mghs15 <40787295+mghs15@users.noreply.github.com>
2021-10-26 23:19:53 +0900

    Update mesh_bbox.js

--- tools/chiikimesh/mesh_bbox.js
+++ tools/chiikimesh/mesh_bbox.js
@@ -1,35 +1,77 @@
 //関数-------------------------------------
 
-var bboxMesh = (mesh) => {
+const mesh2bbox = (mesh) => {
 
-  if(!mesh.match(/^\d\d\d\d\d\d$/)){
-    console.log("Warning: Argument is expexted 6 digits integer.");
+  if(mesh.match(/^\d\d\d\d\d\d$/)){ //2次メッシュ
+    const X = +mesh.substr(0, 2);
+    const Y = +mesh.substr(2, 2);
+    const x = +mesh.substr(4, 1);
+    const y = +mesh.substr(5, 1);
+    
+    //console.log(X, Y, x, y);
+    
+    const minlon = (+Y + 100) + (  +y      * (7.5/60) );
+    const maxlon = (+Y + 100) + ( (+y + 1) * (7.5/60) );
+    const maxlat = (+X / 1.5) + ( (+x + 1) * (5/60) );
+    const minlat = (+X / 1.5) + (  +x      * (5/60) );
+    
+    const bbox = [maxlon, maxlat, minlon, minlat];
+    
+    return bbox;    
+    
+  }else if(mesh.match(/^\d\d\d\d\d\d\d\d$/)){
+    const X  = +mesh.substr(0, 2);
+    const Y  = +mesh.substr(2, 2);
+    const x  = +mesh.substr(4, 1);
+    const y  = +mesh.substr(5, 1);
+    const xx = +mesh.substr(6, 1);
+    const yy = +mesh.substr(7, 1);
+      
+    //console.log(X, Y, x, y);
+    
+    const minlon = (+Y + 100) + (  +y * (7.5/60) ) + (  +yy      * (0.75/60));
+    const maxlon = (+Y + 100) + (  +y * (7.5/60) ) + ( (+yy + 1) * (0.75/60));
+    const maxlat = (+X / 1.5) + (  +x * (5/60) )   + ( (+xx + 1) * (0.5/60));
+    const minlat = (+X / 1.5) + (  +x * (5/60) )   + (  +xx      * (0.5/60));
+    
+    const bbox = [maxlon, maxlat, minlon, minlat];
+    
+    return bbox;
+    
+  }else{
+    console.log("Warning: Argument is expexted 6 or 8 digits integer.");
     return;
   }
-  
-  var X = +mesh.substr(0, 2);
-  var Y = +mesh.substr(2, 2);
-  var x = +mesh.substr(4, 1);
-  var y = +mesh.substr(5, 1);
-  
-  //console.log(X, Y, x, y);
-  
-  var minlon = (+Y + 100) + (  +y      * (7.5/60) );
-  var maxlon = (+Y + 100) + ( (+y + 1) * (7.5/60) );
-  var maxlat = (+X / 1.5) + ( (+x + 1) * (5/60) );
-  var minlat = (+X / 1.5) + (  +x      * (5/60) );
-  
-  var bbox = [maxlon, maxlat, minlon, minlat];
-  
-  return bbox;
 
 }
 
+const bbox2poly = (bbox, prop = {}) => {
+  
+  return {
+      "type": "Feature",
+      "properties": prop,
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [
+          [bbox[2], bbox[1]],
+          [bbox[0], bbox[1]],
+          [bbox[0], bbox[3]],
+          [bbox[2], bbox[3]],
+          [bbox[2], bbox[1]]
+        ]
+      }
+  };
+  
+}
+
+
+
 //実行---------------------------------
 
-var mesh = process.argv[2];
+const mesh = process.argv[2];
 
-var bbox = bboxMesh(mesh);
+const bbox = mesh2bbox(mesh);
 
-console.log(bbox);
+const poly = bbox2poly(bbox, {"mesh": mesh});
 
+console.log(poly);
